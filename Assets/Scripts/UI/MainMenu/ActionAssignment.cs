31ccb3a398151ee56389eedfcdecc5bcf3bb2473
Gavin Lochtefeld <gavin.loch@gmail.com>
2020-10-11 19:11:11 -0500

    Created a temporary main menu system.
    Most of the work is in place for the animated title, need to finish options / credits
    
    Also, this branch name is inaccurate, since I've been adding some art assets that I made

---
+++ Assets/Scripts/UI/MainMenu/ActionAssignment.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace WOB.UI
+{
+    public class ActionAssignment : MonoBehaviour
+    {
+        #region Serialized Fields
+#pragma warning disable CS0649
+        public CanvasSwitcher _switcher;
+        [Header("Main Buttons")]
+        public Button _start;
+        public Button _toOptions;
+        public Button _toCredits;
+        public Button _quit;
+        [Header("Back to Main buttons")]
+        public Button _fromOptionsToStart;
+        public Button _fromCreditsToStart;
+#pragma warning restore CS0649
+        #endregion
+
+        #region Monobehaviour
+        void Start()
+        {
+            _start.onClick.AddListener(() => SceneManager.LoadScene(1));
+            _toOptions.onClick.AddListener(() => _switcher.SwitchCanvas(_switcher[1]));
+            _toCredits.onClick.AddListener(() => _switcher.SwitchCanvas(_switcher[2]));
+            _fromOptionsToStart.onClick.AddListener(() => _switcher.SwitchCanvas(_switcher[0]));
+            _fromCreditsToStart.onClick.AddListener(() => _switcher.SwitchCanvas(_switcher[0]));
+            _quit.onClick.AddListener(() => Application.Quit());
+        }
+        #endregion
+    }
+}

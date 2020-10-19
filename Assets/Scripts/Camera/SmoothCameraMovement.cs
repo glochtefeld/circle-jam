b7e82ae7313f62c8bcb95067c89077890ca7cf6f
oymk <72468295+omaroymk@users.noreply.github.com>
2020-10-19 17:50:38 +0200

    these are the changes

---
+++ Assets/Scripts/Camera/SmoothCameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraMovement : MonoBehaviour
+{
+    #region Serialized Fields
+    [Header("Follow")]
+    [Tooltip("Insert The GameObject You Want To Follow")]
+    public GameObject Player;
+    [Space]
+    public Vector3 PlayerPosition;
+    [Space]
+    public Vector3 Velocity;
+    [Space]
+    public float Smoothness;
+    #endregion
+
+
+
+
+    void Start()
+    {
+        Velocity = Vector3.zero;
+    }
+
+
+    void Update()
+    {
+        PlayerPosition = new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, PlayerPosition, ref Velocity, Smoothness);
+    }
+}

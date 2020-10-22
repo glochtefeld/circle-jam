3324e25ea4cce8ece7edea515af6114658b3a38a
Gavin Lochtefeld <gavin.loch@gmail.com>
2020-10-22 17:53:44 -0500

    Initial version of credits

--- Assets/Scripts/UI/Credits/CreditFX.cs
+++ Assets/Scripts/UI/Credits/CreditFX.cs
@@ -1,18 +1,20 @@
 ﻿// #define DIAGNOSTIC_MODE
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditFX : MonoBehaviour
 {
-#region Serialized Fields
+    #region Serialized Fields
 #pragma warning disable CS0649
-
+    public GameObject creditsPanel;
 #pragma warning restore CS0649
 #endregion
 
 #region Monobehaviour
     void Start()
     {
-        
+        StartCoroutine(ScrollupSlowly());
     }
 
     void Update()
@@ -20,6 +22,25 @@ public class CreditFX : MonoBehaviour
         
     }
 #endregion
+
+    private IEnumerator ScrollupSlowly()
+    {
+        yield return new WaitForSeconds(3f);
+        var time = 0f;
+        while (time < 60f)
+        {
+            time += Time.fixedDeltaTime;
+            creditsPanel.transform.position =
+                new Vector3(
+                    creditsPanel.transform.position.x,
+                    creditsPanel.transform.position.y + 0.003f,
+                    creditsPanel.transform.position.z);
+            yield return null;
+        }
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(0);
+
+    }
 
 #if DIAGNOSTIC_MODE
     private void OnGUI()
